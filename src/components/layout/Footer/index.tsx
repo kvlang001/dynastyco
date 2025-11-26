60229f838630780f308afe6f931753a48073207d
kvlang001 <156228294+kvlang001@users.noreply.github.com>
2025-11-26 13:38:30 +0000

    COMMIT

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -74,7 +74,7 @@ const Footer = () => {
                   "text-[28px] lg:text-[32px] mb-6",
                 ])}
               >
-                SHOP.CO
+                DYNASTY.CO
               </h1>
               <p className="text-black/60 text-sm mb-9">
                 We have clothes that suits your style and which you’re proud to
@@ -103,12 +103,12 @@ const Footer = () => {
           <hr className="h-[1px] border-t-black/10 mb-6" />
           <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center mb-2">
             <p className="text-sm text-center sm:text-left text-black/60 mb-4 sm:mb-0 sm:mr-1">
-              Shop.co © Made by{" "}
+              Dynasty.co © 2025 Made by{" "}
               <Link
                 href="https://github.com/mohammadoftadeh"
                 className="text-black font-medium"
               >
-                Mohammad Oftadeh
+                DYNASTY LLC
               </Link>
               {", "}
               Designed by{" "}
@@ -116,7 +116,7 @@ const Footer = () => {
                 href="https://www.figma.com/@hamzauix"
                 className="text-black font-medium"
               >
-                Hamza Naeem
+                FTG XANDER
               </Link>
             </p>
             <div className="flex items-center">
